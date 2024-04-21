379a4bf53e57dd5407f5e08a45d12733ed3ca9e9
m.laic <m.laic@hotmail.com>
2024-04-21 18:42:18 +0200

    add CustomerDao for save delete and getAllCustomer

---
+++ ServerInSpring/src/main/java/com/exam2app/ServerInSpring/model/customer/CustomerDao.java
@@ -0,0 +1,31 @@
+package com.exam2app.ServerInSpring.model.customer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.util.Streamable;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CustomerDao {
+    @Autowired // Dependency injection
+    private CustomerRepository repository;
+
+    public void save(Customer customer) {
+        repository.save(customer);
+    }
+
+    public List<Customer> getAllCustomer() {
+        List<Customer> customers = new ArrayList<>();
+        Streamable.of(repository.findAll())
+                .forEach(customers::add);
+        return customers;
+    }
+
+    public void delete(Customer customer) {
+        repository.delete(customer);
+    }
+
+
+}
