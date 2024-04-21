4161f825073cf622151b9b7ff960037b7d455be5
m.laic <m.laic@hotmail.com>
2024-04-21 17:49:17 +0200

    error embedded Id in class Customer, fix imported Id from jarkarta,

--- ServerInSpring/src/main/java/com/exam2app/ServerInSpring/ServerInSpringApplication.java
+++ ServerInSpring/src/main/java/com/exam2app/ServerInSpring/ServerInSpringApplication.java
@@ -1,11 +1,12 @@
 package com.exam2app.ServerInSpring;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.exam2app.ServerInSpring")// from witch package it will read data
+@EnableAutoConfiguration // add config
 public class ServerInSpringApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ServerInSpringApplication.class, args);
 	}
