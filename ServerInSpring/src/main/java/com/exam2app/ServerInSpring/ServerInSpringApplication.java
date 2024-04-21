985ebcc8f48dbae82a99f015eeb06b2cb305b2e4
m.laic <m.laic@hotmail.com>
2024-04-21 16:31:57 +0200

    initial commit

---
+++ ServerInSpring/src/main/java/com/exam2app/ServerInSpring/ServerInSpringApplication.java
@@ -0,0 +1,13 @@
+package com.exam2app.ServerInSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerInSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerInSpringApplication.class, args);
+	}
+
+}
---
+++ ServerInSpring/src/test/java/com/exam2app/ServerInSpring/ServerInSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam2app.ServerInSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerInSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
